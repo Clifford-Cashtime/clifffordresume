fce5df808a516bb538c7559ff1e6091a4bc3f94d
Clifford-Cashtime <70904365+Clifford-Cashtime@users.noreply.github.com>
2020-10-11 15:38:22 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,99 +1,196 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-};
-const express = require('express');
+var express = require("express");
+var app = express();
+var cfenv = require("cfenv");
+var bodyParser = require('body-parser')
 
-const app = express();
-const bcrypt = require('bcrypt');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
 
-const passport = require('passport');
-const flash = require('express-flash');
-const session = require('express-session');
-const methodOverride = require('method-override');
+// parse application/json
+app.use(bodyParser.json())
 
+let mydb, cloudant;
+var vendor; // Because the MongoDB and Cloudant use different API commands, we
+            // have to check which command should be used based on the database
+            // vendor.
+var dbName = 'mydb';
 
-const initializePassport = require('./passport-config');
-initializePassport(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-);
+// Separate functions are provided for inserting/retrieving content from
+// MongoDB and Cloudant databases. These functions must be prefixed by a
+// value that may be assigned to the 'vendor' variable, such as 'mongodb' or
+// 'cloudant' (i.e., 'cloudantInsertOne' and 'mongodbInsertOne')
 
-const users = [];
+var insertOne = {};
+var getAll = {};
 
-app.engine('html', require('ejs').renderFile);
-app.set('view-engine', 'ejs');
-app.set('view engine', 'html');
-app.use(express.static('views'));
-app.use(express.urlencoded({ extended: false }));
-app.use(flash());
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(methodOverride('_method'));
-
-app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.html', { name: req.user.name })
-
-})
-
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
-})
-
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
-
-app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs')
-})
-
-app.post('/register', checkNotAuthenticated, async(req, res) => {
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        res.redirect('/login'); {}
-    } catch {
-        res.redirect('/register');
-
+insertOne.cloudant = function(doc, response) {
+  mydb.insert(doc, function(err, body, header) {
+    if (err) {
+      console.log('[mydb.insert] ', err.message);
+      response.send("Error");
+      return;
     }
-    console.log(users);
-})
-
-app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/login')
-})
-
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next()
-    }
-
-    return res.redirect('/login')
+    doc._id = body.id;
+    response.send(doc);
+  });
 }
 
-
-//Code for protecting all the routes
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect('/')
+getAll.cloudant = function(response) {
+  var names = [];  
+  mydb.list({ include_docs: true }, function(err, body) {
+    if (!err) {
+      body.rows.forEach(function(row) {
+        if(row.doc.name)
+          names.push(row.doc.name);
+      });
+      response.json(names);
     }
-    next()
+  });
+  //return names;
 }
-let port = process.env.PORT || 9922;
 
-app.listen(9922);
+let collectionName = 'mycollection'; // MongoDB requires a collection name.
+
+insertOne.mongodb = function(doc, response) {
+  mydb.collection(collectionName).insertOne(doc, function(err, body, header) {
+    if (err) {
+      console.log('[mydb.insertOne] ', err.message);
+      response.send("Error");
+      return;
+    }
+    doc._id = body.id;
+    response.send(doc);
+  });
+}
+
+getAll.mongodb = function(response) {
+  var names = [];
+  mydb.collection(collectionName).find({}, {fields:{_id: 0, count: 0}}).toArray(function(err, result) {
+    if (!err) {
+      result.forEach(function(row) {
+        names.push(row.name);
+      });
+      response.json(names);
+    }
+  });
+}
+
+/* Endpoint to greet and add a new visitor to database.
+* Send a POST request to localhost:3000/api/visitors with body
+* {
+*   "name": "Bob"
+* }
+*/
+app.post("/api/visitors", function (request, response) {
+  var userName = request.body.name;
+  var doc = { "name" : userName };
+  if(!mydb) {
+    console.log("No database.");
+    response.send(doc);
+    return;
+  }
+  insertOne[vendor](doc, response);
+});
+
+/**
+ * Endpoint to get a JSON array of all the visitors in the database
+ * REST API example:
+ * <code>
+ * GET http://localhost:3000/api/visitors
+ * </code>
+ *
+ * Response:
+ * [ "Bob", "Jane" ]
+ * @return An array of all the visitor names
+ */
+app.get("/api/visitors", function (request, response) {
+  var names = [];
+  if(!mydb) {
+    response.json(names);
+    return;
+  }
+  getAll[vendor](response);
+});
+
+// load local VCAP configuration  and service credentials
+var vcapLocal;
+try {
+  vcapLocal = require('./vcap-local.json');
+  console.log("Loaded local VCAP", vcapLocal);
+} catch (e) { }
+
+const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
+
+const appEnv = cfenv.getAppEnv(appEnvOpts);
+
+if (appEnv.services['compose-for-mongodb'] || appEnv.getService(/.*[Mm][Oo][Nn][Gg][Oo].*/)) {
+  // Load the MongoDB library.
+  var MongoClient = require('mongodb').MongoClient;
+
+  dbName = 'mydb';
+
+  // Initialize database with credentials
+  if (appEnv.services['compose-for-mongodb']) {
+    MongoClient.connect(appEnv.services['compose-for-mongodb'][0].credentials.uri, null, function(err, db) {
+      if (err) {
+        console.log(err);
+      } else {
+        mydb = db.db(dbName);
+        console.log("Created database: " + dbName);
+      }
+    });
+  } else {
+    // user-provided service with 'mongodb' in its name
+    MongoClient.connect(appEnv.getService(/.*[Mm][Oo][Nn][Gg][Oo].*/).credentials.uri, null,
+      function(err, db) {
+        if (err) {
+          console.log(err);
+        } else {
+          mydb = db.db(dbName);
+          console.log("Created database: " + dbName);
+        }
+      }
+    );
+  }
+
+  vendor = 'mongodb';
+} else if (appEnv.services['cloudantNoSQLDB'] || appEnv.getService(/[Cc][Ll][Oo][Uu][Dd][Aa][Nn][Tt]/)) {
+  // Load the Cloudant library.
+  var Cloudant = require('@cloudant/cloudant');
+
+  // Initialize database with credentials
+  if (appEnv.services['cloudantNoSQLDB']) {
+    // CF service named 'cloudantNoSQLDB'
+    cloudant = Cloudant(appEnv.services['cloudantNoSQLDB'][0].credentials);
+  } else {
+     // user-provided service with 'cloudant' in its name
+     cloudant = Cloudant(appEnv.getService(/cloudant/).credentials);
+  }
+} else if (process.env.CLOUDANT_URL){
+  cloudant = Cloudant(process.env.CLOUDANT_URL);
+}
+if(cloudant) {
+  //database name
+  dbName = 'mydb';
+
+  // Create a new "mydb" database.
+  cloudant.db.create(dbName, function(err, data) {
+    if(!err) //err if database doesn't already exists
+      console.log("Created database: " + dbName);
+  });
+
+  // Specify the database we are going to use (mydb)...
+  mydb = cloudant.db.use(dbName);
+
+  vendor = 'cloudant';
+}
+
+//serve static file (index.html, images, css)
+app.use(express.static(__dirname + '/views'));
+
+
+
+var port = process.env.PORT || 3000
+app.listen(port, function() {
+    console.log("To view your app, open this link in your browser: http://localhost:" + port);
+});
